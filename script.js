f8c82c1129e77f8a6c1ca484d6f6471f1b208bf0
Sayon Dutta <52020690+Sayon2000@users.noreply.github.com>
2023-07-13 05:36:32 +0530

    filtering mechanism made

--- script.js
+++ script.js
@@ -2,17 +2,21 @@ let form = document.getElementById('addForm')
 
 form.addEventListener('submit' , addItem)
 let lists = document.getElementById('items')
+let filter = document.getElementById('filter')
 
+filter.addEventListener('keyup' , filterItems)
 lists.addEventListener('click' , handleClick)
 //      <button type="button" class="btn btn-primary btn-sm mx-2">edit</button>
 function addItem(e){
     e.preventDefault();
     if(e.target.firstElementChild.value.length !== 0){
 
-    
+
     let li = document.createElement('li')
     li.className ='list-group-item'
     li.appendChild(document.createTextNode(e.target.firstElementChild.value))
+    li.appendChild(document.createTextNode(" " +
+     e.target.firstElementChild.nextElementSibling.value))
     let div = document.createElement('div')
     div.className= 'float-right'
     let edit = document.createElement('button')
@@ -26,6 +30,8 @@ function addItem(e){
     li.appendChild(div)
     lists.appendChild(li)
     console.log(li)
+    e.target.firstElementChild.value = ""
+    e.target.firstElementChild.nextElementSibling.value = ""
 }
 }
 
@@ -35,4 +41,15 @@ function handleClick(e){
         let li  = e.target.parentNode.parentNode
         lists.removeChild(li)
     }
+}
+
+function filterItems(e){
+    console.log(e.target.value)
+    let items = lists.getElementsByTagName('li')
+    Array.from(items).forEach((item)=>{
+        if(item.textContent.toLowerCase().indexOf(e.target.value.toLowerCase()) != -1)
+            item.style.display = 'block'
+        else 
+            item.style.display = 'none'
+    })
 }
